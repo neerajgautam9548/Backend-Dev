62e9d3d3755eb8f3da17716c9cf0d6389df5b9ba
neerajgautam9548 <neerajgautam9548@gmail.com>
2026-01-26 16:53:57 +0530

    Practice work updates

--- practice/index.js
+++ practice/index.js
@@ -1,16 +1,24 @@
-const express=require('express');
+const express = require("express");
+const fs = require("fs");
 
-function add(a,b){
-    return a+b;
+const app = express();
+
+// Functions
+function add(a, b) {
+  return a + b;
 }
-function remove(a,b){
-    return a+b;
+
+function remove(a, b) {
+  return a - b;
 }
-module.exports={add,remove};
 
-FileSystem.appendFileSync("test.txt",new Date().toLocaleString());
-const file=FileSystem.readFileSync("test.txt","utf-8")
+module.exports = { add, remove };
 
-index.listen(3000,()=>{
-    console.log("server is running on port : 3000");
-})
+// File system work
+fs.appendFileSync("test.txt", new Date().toLocaleString() + "\n");
+const file = fs.readFileSync("test.txt", "utf-8");
+
+// Server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
